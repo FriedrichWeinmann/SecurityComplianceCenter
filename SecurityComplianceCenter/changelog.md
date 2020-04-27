cafcacac2c312262a8da7d82fd248ec9838d8de3
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2020-04-27 12:23:47 +0200

    new command

--- SecurityComplianceCenter/changelog.md
+++ SecurityComplianceCenter/changelog.md
@@ -1,5 +1,7 @@
 ﻿# Changelog
-## 1.0.0 (2020-04-14)
- - New: Some Stuff
- - Upd: Moar Stuff
- - Fix: Much Stuff
+
+## 1.0.1 (2020-04-27)
+
+## 1.0.0 (2020-04-27)
+
+- Initial Release
---
+++ SecurityComplianceCenter/functions/Import-SccLabelLocalizationXml.ps1
@@ -0,0 +1,59 @@
+﻿function Import-SccLabelLocalizationXml {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+		[Alias('FullName')]
+		[string[]]
+		$Path
+	)
+
+	begin {
+		$defaultProcessed = @{ }
+	}
+	process {
+		foreach ($pathItem in $Path) {
+			try { $resolvedPaths = Resolve-PSFPath -Path $pathItem -Provider FileSystem }
+			catch {
+				#TODO: Add error handling
+			}
+
+			foreach ($fileItem in $resolvedPaths) {
+				try { [xml]$xmlData = Get-Content -Path $fileItem -ErrorAction Stop -Encoding utf8 }
+				catch {
+					#TODO: Add error handling
+				}
+				$language = $xmlData.Language.Id
+				if (-not $language) {
+					#TODO: Add error handling
+				}
+				
+				foreach ($entry in $xmlData.Language.LocItem)
+				{
+					if ($entry.ID -notmatch '^labelGroups/Sensitivity/labels/.+/(DisplayName|Description)$') { continue }
+	
+					$type = 'Tooltip'
+					if ($entry.ID -Match 'DisplayName$') { $type = 'DisplayName' }
+					[PSCustomObject]@{
+						Name = ($entry.ID -split "/")[-2]
+						Identity = $entry.ID.Replace("labelGroups/Sensitivity/labels/", "").Replace("subLabels/", "").Replace("/","\") -replace '\\(DisplayName|Description)$'
+						Type = $type
+						Language = $language
+						Text = $entry.LocalizedText
+					}
+	
+					if ($defaultProcessed[$entry.ID]) { continue }
+	
+					[PSCustomObject]@{
+						Name = ($entry.ID -split "/")[-2]
+						Identity = $entry.ID.Replace("labelGroups/Sensitivity/labels/", "").Replace("subLabels/", "").Replace("/","\") -replace '\\(DisplayName|Description)$'
+						Type = $type
+						Language = 'default'
+						Text = $entry.defaultText
+					}
+	
+					$defaultProcessed[$entry.ID] = $true
+				}
+			}
+		}
+	}
+}
